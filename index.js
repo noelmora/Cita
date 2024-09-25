44e8b5ebbf5995e2598fe870e52b4447c0a6c48e
noelmora <162498510+noelmora@users.noreply.github.com>
2024-09-24 21:26:30 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const axios = require('axios');
 const path = require('path');
 
 const app = express();
-const port = 3000;
 
-// Servir archivos estáticos (HTML, CSS, JS)
+
+const port = process.env.PORT || 3000;
+
+
 app.use(express.static('public'));
 
-// Ruta para obtener una cita motivacional
-app.get('/quote', async (req, res) => {
-    try {
-      const response = await axios.get('https://zenquotes.io/api/random');
-      const quote = response.data[0].q;
-      const author = response.data[0].a;
-      res.json({ quote, author });
-    } catch (error) {
-      console.error('Error al obtener la cita:', error);
-      res.status(500).json({ error: 'No se pudo obtener la cita' });
-    }
-  });
 
-// Iniciar el servidor
+app.get('/quote', async (req, res) => {
+  try {
+    const response = await axios.get('https://zenquotes.io/api/random');
+    const quote = response.data[0].q;
+    const author = response.data[0].a;
+    res.json({ quote, author });
+  } catch (error) {
+    console.error('Error al obtener la cita:', error);
+    res.status(500).json({ error: 'No se pudo obtener la cita' });
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
